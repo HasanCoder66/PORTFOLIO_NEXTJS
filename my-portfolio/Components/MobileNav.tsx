a4a4cedea41af74c0784d2f8bfe1e77ebf15c968
HasanCoder66 <hasanashraf874@gmail.com>
2024-01-04 21:02:46 +0500

    About Working Start

--- my-portfolio/Components/MobileNav.tsx
+++ my-portfolio/Components/MobileNav.tsx
@@ -11,8 +11,8 @@ const MobileNav = ({nav , closeNav}:Props) => {
     // console.log(nav);
     // console.log(navAnimated);
   return (
-    <div className={`fixed ${navAnimated}  transform transition-all duration-300 top-0 bottom-0 left-0 right-0 z-[10000] bg-[#09101a]`}>
-      <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center">
+    <div className={`fixed ${navAnimated}  transform transition-all duration-300 top-0 bottom-0 left-0 right-0 z-[1000000] bg-[#09101a]`}>
+      <div className="flex z-[1000000] flex-col w-[100vw] h-[100vh] justify-center items-center">
         <div className="nav-link-mobile">HOME</div>
         <div className="nav-link-mobile">ABOUT</div>
         <div className="nav-link-mobile">SERVICES</div>
