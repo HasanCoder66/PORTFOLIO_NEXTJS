e19a4bf4230340234ac99004c67d88c56d6e24bb
HasanCoder66 <hasanashraf874@gmail.com>
2024-01-04 12:09:18 +0500

    close bar not working

--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -1,12 +1,25 @@
-import Nav from "@/Components/Nav"
+import MobileNav from "@/Components/MobileNav";
+import Nav from "@/Components/Nav";
+import React from "react";
 
 const HomePage = () => {
-  return <div className= "overflow-x-hidden">
-    <div>
-      <Nav />
-    </div>
-  </div>
-
+const [nav , setNav ] = React.useState(true)
+const openNav = () => {
+  console.log('open nav chal raha hai')
+  setNav(true)
 }
+const closeNav = () => {
+  console.log('close nav chal raha hai');
+  setNav(false)
+}
+  return (
+    <div className="overflow-x-hidden">
+      <div>
+        <MobileNav nav={nav} closeNav={closeNav} />
+        <Nav openNav={openNav} />
+      </div>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
