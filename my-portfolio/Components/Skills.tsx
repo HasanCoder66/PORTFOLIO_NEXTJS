3360abec74baf1221c8fadcbef714ab6e5468274
HasanCoder66 <hasanashraf874@gmail.com>
2024-01-11 15:13:07 +0500

    error comes in skills cmp

--- my-portfolio/Components/Skills.tsx
+++ my-portfolio/Components/Skills.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import SkillsItem from './SkillsItem';
+import SkillLanguages from './SkillLanguages';
 
 const Skills = () => {
   return (
-    <div>Skills</div>
-  )
+    // <>
+    <>
+      // <>
+      <div className="pt-[4rem] md:pt-[8rem] pb-[5rem] bg-[#09101a]">
+        <h1 className="heading"> Education & <span className="text-yellow-400">Skills</span></h1>
+      </div><div className="pt-[4rem] md:pt-[8rem] mx-auto h-[88%] grid grid-cols-1 md:grid-cols-2  gap-[2rem]items-center ">
+        <div>
+          <SkillsItem />
+          <SkillsItem />
+          <SkillLanguages />
+        </div>
+        <div>
+          <SkillsItem />
+          <SkillsItem />
+          <SkillLanguages />
+        </div>
+      </div></>
+    // </>
+  );
 }
 
 export default Skills
