f4cd3671f5c8d1c035eab5148311b5d2b12087f3
HasanCoder66 <hasanashraf874@gmail.com>
2024-01-12 21:21:09 +0500

    working

--- my-portfolio/Components/Skills.tsx
+++ my-portfolio/Components/Skills.tsx
@@ -4,13 +4,13 @@ import SkillLanguages from './SkillLanguages';
 
 const Skills = () => {
   return (
-    // <>
-    <>
-      // <>
+      <>
       <div className="pt-[4rem] md:pt-[8rem] pb-[5rem] bg-[#09101a]">
         <h1 className="heading"> Education & <span className="text-yellow-400">Skills</span></h1>
       </div><div className="pt-[4rem] md:pt-[8rem] mx-auto h-[88%] grid grid-cols-1 md:grid-cols-2  gap-[2rem]items-center ">
         <div>
+          <SkillsItem />
+          <SkillsItem />
           <SkillsItem />
           <SkillsItem />
           <SkillLanguages />
@@ -21,7 +21,6 @@ const Skills = () => {
           <SkillLanguages />
         </div>
       </div></>
-    // </>
   );
 }
 
