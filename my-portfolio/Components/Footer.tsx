544898c1e16c83b9b644f532aeaf982b6a16fcce
HasanCoder66 <hasanashraf874@gmail.com>
2024-08-01 05:10:56 +0500

    address set in footer

--- my-portfolio/Components/Footer.tsx
+++ my-portfolio/Components/Footer.tsx
@@ -1,4 +1,4 @@
-import { DevicePhoneMobileIcon, MapIcon } from "@heroicons/react/20/solid";
+import { DevicePhoneMobileIcon, EnvelopeIcon, EnvelopeOpenIcon, MapIcon } from "@heroicons/react/20/solid";
 import React from "react";
 
 const Footer = () => {
@@ -13,8 +13,8 @@ const Footer = () => {
             <h1 className="text-[25px] mb-[0.2rem] font-semibold text-white">
               Address
             </h1>
-            <p className="text-[17px] opacity-60 w-[90%] text-white">
-              Lorem, ipsum dolor sit
+            <p className="text-[15px] opacity-60 w-[100%] text-white">
+              R-40 Al-Noor Society <br /> Near Ahsanabad <br />Scheme, 33 Karachi Pakistan.
             </p>
           </div>
         </div>
@@ -27,12 +27,25 @@ const Footer = () => {
             <h1 className="text-[25px] mb-[0.2rem] font-semibold text-white">
               Phone
             </h1>
-            <p className="text-[17px] opacity-60 w-[90%] text-white">
+            <p className="text-[15px] opacity-60 w-[90%] text-white">
               +923170243866
             </p>
           </div>
         </div>
-        <div></div>
+
+        <div className="flex items-center space-x-6">
+          <div className="md:w-[6.5rem] md:h-[6.5rem] w-[6rem] h-[5rem] flex items-center justify-center rounded-full bg-[#55e6a5]">
+            <EnvelopeIcon className="md:w-[4rem] md:h-[4rem] w-[3.5rem] h-[3.5rem] text-black" />
+          </div>
+          <div>
+            <h1 className="text-[25px] mb-[0.2rem] font-semibold text-white">
+              Send Me Email
+            </h1>
+            <p className="text-[15px] opacity-60 w-[90%] text-white">
+              codermhasan@gmail.com
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
